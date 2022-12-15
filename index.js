d9684b7a1f5006482130d336190d29d0fdf76f64
chetung2808 <58966385+chetung2808@users.noreply.github.com>
2022-12-15 12:33:25 +0700

    update

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const connect = async () => {
 
     try {
 
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            } );
         console.log("Connected to DB");
     
     } catch(error) {
